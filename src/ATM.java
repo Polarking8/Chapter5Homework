75b6ca17db56fad46f5f08efd52e6cfdaa5d6fb1
Polarking8 <57197175+Polarking8@users.noreply.github.com>
2022-01-12 20:58:52 -0700

    Attempt to fix grade by fixing syntax and other things.

--- src/ATM.java
+++ src/ATM.java
@@ -1,23 +1,27 @@
 public class ATM {
-    private int numberOfTries;
+    private int numberOfTries = 0;
+    boolean blocked = false;
+    boolean accessGranted = false;
     public ATM(){
+
     }
     /**
      * checks if input is the PIN, if it's not, then check if number of tries is less than 2
      * (3 because java counts from 0)
      * otherwise will block bank card.
      * @param input is the PIN the user gives
+     * @return
      */
-    public void enterPinNumber(int input){
+    public void enterPinNumber(double input) {
         if (input == 1234) {
             System.out.println("Your PIN is correct");
-            System.exit(0);
-        } else if (numberOfTries < 2) {
+            accessGranted = true;
+        } else if (numberOfTries >= 2) {
+            System.out.println("Your bank card is blocked");
+            blocked = true;
+        } else{
             System.out.println("Your PIN is incorrect");
             numberOfTries++;
-        } else {
-            System.out.println("Your bank card is blocked");
-            System.exit(0);
         }
     }
 }
--- src/ATMTester.java
+++ src/ATMTester.java
@@ -1,3 +1,7 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
 import java.util.Scanner;
 
 public class ATMTester {
@@ -5,22 +9,19 @@ public class ATMTester {
         ATM machine;
         machine = new ATM();
         Scanner in = new Scanner(System.in);
-        for (int i = 0; i<3;){
+        for (int i = 0; i < 3;){
             System.out.print("Enter your PIN: ");
-            int input = in.nextInt();
+            double input = in.nextInt();
             machine.enterPinNumber(input);
+            if (machine.accessGranted == true){
+                System.exit(0);
+            } else if (machine.blocked == true){
+                System.exit(0);
+            }
             i++;
         }
         System.out.println("PIN should be 1234, if it's not, then ask again for 2 more times.");
-        // alternatively you can have this iterate 3 times, like this:
-        // System.out.print("Enter your PIN: ");
-        // int input = in.nextInt();
-        // machine.enterPinNumber(input);
-        // System.out.print("Enter your PIN: ");
-        // int input2 = in.nextInt();
-        // machine.enterPinNumber(input2);
-        // System.out.print("Enter your PIN: ");
-        //int input3 = in.nextInt();
-        // machine.enterPinNumber(input3);
     }
+
+
 }
